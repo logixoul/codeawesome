d950fdff1d20e67f3924912fbd6670c051742e5a
Стефан Монов <logixoul@gmail.com>
2026-01-19 12:20:08 +0200

    .

--- ot viktor/svelte/src/lib/components/Card.svelte
+++ ot viktor/svelte/src/lib/components/Card.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-    const linkTitle = "click here";
+    //const linkTitle = "click here";
+    let { id, title, description } = $props();
 </script>
 
-<h1>Hi! <a href=".">{linkTitle.toUpperCase()}</a></h1>
+<a
+    href="."
+    class="card"
+    data-title="{id}"
+    target="_blank"
+    rel="noopener noreferrer"
+    >
+    <div class="card-inner">
+        <div class="card-icon">
+            <img src="/images/icon-{id}.png" alt={id} loading="lazy" />
+        </div>
+        <div class="card-content">
+            <h2 class="card-title">{title}</h2>
+            <p class="card-description">{description}</p>
+        </div>
+    </div>
+    <div class="card-border"></div>
+</a>
 
 <style>
-    h1 { color: red; }
+    /*.card-title { color: red !important; }*/
 </style>
