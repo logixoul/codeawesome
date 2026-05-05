7f8ccd2cd92ad175e8b4a2f9126d70f43e865179
Stefan Monov <logixoul@gmail.com>
2026-05-05 12:28:20 +0300

    fixed failing build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -85,7 +85,7 @@
 				onclick={setCurrentSlideout}
 			></ProjectShowcaseBox>
             <ProjectShowcaseBox
-				title="Мултимащабен растеж на кристали"
+				title="Мултимащабно анти-изглаждане"
                 videoUrl="http://nesuho.stefanteaches.eu/videos/multiscale-growth-sharp.mp4"
 				description="
                     <p>Визуален експеримент. Работи едновременно на различни мащаби - фин, среден, едър.</p>
